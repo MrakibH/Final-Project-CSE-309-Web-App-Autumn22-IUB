1614f4357c32e7dfb58cada1a12cf5549372e1c1
MrakibH <116109041+MrakibH@users.noreply.github.com>
2022-12-21 13:22:42 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +88 01924148241 </p>
+         <p> <i class="fas fa-phone"></i> +88 01767983636 </p>
+         <p> <i class="fas fa-envelope"></i> rakib.rocky@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Dhaka, Bangladesh - 1229 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+   </div>
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Md Rakib Hossain</span> </p>
+</section>
